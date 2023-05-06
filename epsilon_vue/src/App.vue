ca6972e212c64a2f855cb92b2653353d05febb93
Dylanabo <dylan.abodi@epitech.eu>
2023-05-06 18:19:35 +0400

    add ++

--- epsilon_vue/src/App.vue
+++ epsilon_vue/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HomeVue />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HomeVue from './pages/HomeVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeVue
   }
 }
 </script>
